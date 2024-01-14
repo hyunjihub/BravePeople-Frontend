7d72734da0ac84ac2330e123d2a50ad196d3f791
hyunjihub <syub98774@naver.com>
2024-01-14 23:52:12 +0900

    20240114

--- src/ui/header/Header.jsx
+++ src/ui/header/Header.jsx
@@ -2,14 +2,27 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import logo from "./logo.png";
+import { useInView } from "react-intersection-observer"
 
 import Button from "./HeaderButton";
 
 
 const Wrapper = styled.div`
     width : 100vw;
-    height : 60px;
-    border-bottom: 1px solid #333;
+    height : 70px;
+    border-bottom: 1px solid #efefef;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+    background-color: #fff;
+`;
+
+const RightContainer = styled.div`
+    width: 250px;
+    height: 65px;
+    float: right;
+    margin-right: 110px;
+    position: relative;
 `;
 
 const Logo = styled.div`
@@ -17,42 +30,50 @@ const Logo = styled.div`
     height : 43px;
     float : left;
     margin-top : 10px;
-    margin-left : 1vw;
+    margin-left : 3vw;
+    margin-right : 30px;
     cursor: pointer;
 `;
 
 const Location = styled.div`
-    width : 150px;
-    height : 40px;
+    width : 130px;
+    height : 30px;
     float : right;
-    margin-top : 10px;
-    margin-right: 2vw;
     background-color : #fff;
     border-radius : 15px;
     text-align : center;
-    line-height : 40px;
+    line-height : 30px;
     font-weight : bold;
     border: 1px solid #333;
+    font-size : 20px;
+    margin-right: 30px;
+`;
+
+const HiddenLocation = styled.div`
+    width : 130px;
+    height : 30px;
+    background-color : #fff;
+    border: 1px solid #fff;
 `;
 
 const PostListMenu = styled.div`
     width: 120px;
     height: 30px; 
-    font-size: 18px;
+    font-size: 20px;
     text-align: center;
     float: left;
     margin-left: 20px;
-    margin-top: 23px;
+    margin-top: 26px;
     border-radius: 15px;
     background-color: #fff;
-    border: 1px solid #333;
     color: #333;
+    font-weight: bold;
     line-height: 30px;
     cursor: pointer;
     &:hover{
-        background-color: #333;
-        color: #fff;
+        color: #f8332f;
     }
+    letter-spacing: 5px;
 `;
 
 function Header(props) {
@@ -67,20 +88,22 @@ function Header(props) {
         else {
             setLoggedIn(true);
             navigate("/logIn");
-            setTimeout(()=>{navigate("/main")}, 2000);
+            /*setTimeout(()=>{navigate("/main")}, 2000);*/
         }
     };
-
+    
     return (
         <Wrapper>
             <Logo onClick={()=>{navigate("/main");}}>
                 <img src={logo} alt="로고" style={{width:"100%"}}></img>
             </Logo>
-            <PostListMenu onClick={()=>navigate("/helpingPostList")}>원정대게시판</PostListMenu>
-            <PostListMenu onClick={()=>navigate("/helpedPostList")}>의뢰인게시판</PostListMenu>
-            <Button onClick={handleLogOut}>{isLoggedIn?"로그아웃":"로그인"}</Button>
-            {isLoggedIn ? <Button onClick={()=>{navigate("/myPage");}}>마이페이지</Button>: null}   
-            {isLoggedIn ? <Location>위치정보</Location>: null}
+            <PostListMenu onClick={()=>navigate("/postlist/helping")}>원정대</PostListMenu>
+            <PostListMenu onClick={()=>navigate("/postlist/helped")}>의뢰인</PostListMenu>
+            <RightContainer>
+                {isLoggedIn ? <Location>위치정보</Location>: <HiddenLocation />}
+                <Button onClick={handleLogOut}>{isLoggedIn?"로그아웃":"로그인"}</Button>
+                {isLoggedIn ? <Button onClick={()=>{navigate("/mypage");}}>마이페이지</Button>: null}   
+            </RightContainer>
         </Wrapper>
     );
 }
--- src/ui/header/HeaderButton.jsx
+++ src/ui/header/HeaderButton.jsx
@@ -1,26 +1,21 @@
 import styled from "styled-components";
 
+
 const StyledButton = styled.button`
+        font-family: 'SUITE-Regular';
         width : 100px;
         height : 30px;
         float : right;
-        margin-right: 2vw;
-        margin-top: 15px;
         font-size: medium;
-        border-radius: 15px;
         border: none;
         text-align: center;
         cursor: pointer;
         background-color: #fff;
         color: #333;
-        border: 1px solid #333;
-        &:hover {
-            background: #333;
-            color: #fff;
-        }
     `;
 
 function Button(props){
+
     return(
         <StyledButton onClick={props.onClick}>{props.children}</StyledButton>
     );
