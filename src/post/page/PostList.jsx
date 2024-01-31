b9e0f2a0bc05e4809e11233115de78082caf6353
1soo <aaa4799@naver.com>
2024-01-31 12:01:34 +0900

    게시글 간 이동시 게시글 안불러지는 오류 해결

--- src/post/page/PostList.jsx
+++ src/post/page/PostList.jsx
@@ -177,6 +177,7 @@ function PostList(props) {
     useEffect(()=>{
         window.scrollTo(0, 0);
         setLoading(true);
+        setPage(0);
         // 로그인 상태일 때 게시글 조회
         if(JSON.parse(sessionStorage.getItem('savedData')).isLogin){
             axios.get(`http://13.209.77.50:8080/posts?type=${type}&distance=${selectedOption}&page=${page}&amount=7`,
